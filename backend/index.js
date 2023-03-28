44991e559d63e1f68490b8175d76f7a69200ea16
Gaurav Asode <gauravasode5@gmail.com>
2023-03-28 20:26:13 +0530

    Log in Database and regitration route Done

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { connection } = require("./database/db");
+const {userRoute} = require("./route/user.route")
+
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/users",userRoute)
+
+
+
+app.listen(process.env.PORT,async()=>{
+
+    try {
+        await connection
+        console.log("Connected to DB")        
+    } catch (error) {
+        console.log("connot connect to DB")
+    }
+    console.log("server is running")
+
+})
